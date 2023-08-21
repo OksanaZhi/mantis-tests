5e6a3aef691a62fded514064631fa676782dfa31
joa@rio-soft.ru <joa@rio-soft.ru>
2023-08-21 17:21:38 +0500

    Add project files.

---
+++ mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        public RegistrationHelper Registration { get; set; }
+        public FtpHelper Ftp { get;  set; }
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            baseURL = "http://localhost";
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);
+
+
+
+        }
+
+         ~ApplicationManager() 
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (! app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-2.25.7/login_page.php";
+                app.Value = newInstance;
+
+
+            }
+            return app.Value;
+        
+        }
+        
+        public IWebDriver Driver 
+        {
+            get
+            {
+                return driver;
+            }
+        }
+        
+
+               
+    }
+}
